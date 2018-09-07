a5d0d58e409d54ef99b669978bc889d7187000fa
miscsamchris <misc.samchris@gmail.com>
2018-09-07 14:35:57 +0530

    UIsetup1

---
+++ AREDAI/Assets/scripts/loadingbar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class loadingbar : MonoBehaviour {
+    public Transform LoadingBar;
+
+    [SerializeField] private float currentAmount=0;
+    [SerializeField] private float speed=4.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentAmount < 100)
+        {
+            currentAmount += speed * Time.deltaTime;
+            Debug.Log((int)currentAmount);
+        }
+        else
+        {
+            SceneManager.LoadScene("menu");
+                }
+
+        LoadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
+    }
+
+}
--- AREDAI/Assets/scripts/navigation.cs
+++ AREDAI/Assets/scripts/navigation.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class navigation : MonoBehaviour {
-
+    string currentScene,nextscene;
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        currentScene = SceneManager.GetActiveScene().name;
+        switch (currentScene)
+        {
+            case "arloading":nextscene = "armode";
+                break;
+            case "splash":
+                nextscene = "loading";
+                break;
+            case "vrloading":
+                nextscene = "vrmode";
+                break;
+            default:
+                nextscene = "menu";
+                break;
+     
+        }
+        StartCoroutine(changescene(nextscene));
+    }
+    IEnumerator changescene(string s)
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(s);
+    }
 }
