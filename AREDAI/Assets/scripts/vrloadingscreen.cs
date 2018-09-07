0b37addbf48eaf0fbc1dc9439a5689ce1527ee63
miscsamchris <misc.samchris@gmail.com>
2018-09-07 20:31:17 +0530

    VRsetup2

--- AREDAI/Assets/scripts/vrloadingscreen.cs
+++ AREDAI/Assets/scripts/vrloadingscreen.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
 
 public class vrloadingscreen : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        StartCoroutine(VRenable());
+        StartCoroutine(Vrmode());
+    }
+
+    IEnumerator VRenable()
+    {
+        XRSettings.LoadDeviceByName("cardboard");
+        yield return new WaitForSeconds(5);
+        XRSettings.enabled = true;
+    }
+    IEnumerator Vrmode()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("vrmode");
+    }
 }
