c18c8a359165cff8632459827e40fb8bdb0c6bfc
miscsamchris <misc.samchris@gmail.com>
2018-09-07 15:40:04 +0530

    arsetup2

--- AREDAI/Assets/scripts/videoplayer.cs
+++ AREDAI/Assets/scripts/videoplayer.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 using UnityEngine.Video;
 
 public class videoplayer : MonoBehaviour {
-
+    private bool flag=false;
     public GameObject videoscreen;
     private VideoPlayer vp;
+    private Renderer r;
 	// Use this for initialization
 	void Start () {
         vp = videoscreen.GetComponent<VideoPlayer>();
         vp.playOnAwake = false;
+        r = videoscreen.GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetMouseButton(0) && r.enabled)
+        {
+            if (flag)
+            {
+                vp.Play();
+                flag = !flag;
+            }
+            else
+            {
+                vp.Pause();
+                flag = !flag;
+            }
+        }
+        if (r.enabled==false)
+        {
+            vp.Stop();
+        }
 	}
 }
