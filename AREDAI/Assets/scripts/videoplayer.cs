9363fc3b08137f97b2cdc0ae73b17dcb119ec6a3
miscsamchris <misc.samchris@gmail.com>
2018-09-07 15:25:12 +0530

    Arupdate1

---
+++ AREDAI/Assets/scripts/videoplayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class videoplayer : MonoBehaviour {
+
+    public GameObject videoscreen;
+    private VideoPlayer vp;
+	// Use this for initialization
+	void Start () {
+        vp = videoscreen.GetComponent<VideoPlayer>();
+        vp.playOnAwake = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
